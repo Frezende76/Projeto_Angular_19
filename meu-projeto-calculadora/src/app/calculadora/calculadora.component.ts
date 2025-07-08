bc89d9626cf77f95d7ba02c1bab54dc79abb0a6b
Fabricio Rezende <fabriciorez@gmail.com>
2025-07-08 10:44:38 -0300

    Upload: Concluido meu projeto da calculadora

--- meu-projeto-calculadora/src/app/calculadora/calculadora.component.ts
+++ meu-projeto-calculadora/src/app/calculadora/calculadora.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-calculadora',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.scss'
 })
@@ -11,9 +12,36 @@ export class CalculadoraComponent {
 
   numero1: number = 0;
   numero2: number = 0;
-  resultado: number = 0;
+  operadorSelecionado: string = '+';
+  resultado: number | null = null;
+  divisaoPorZero: boolean = false;
+ 
 
   calcularResultado(){
-    this.resultado = this.numero1 + this.numero2;
+    this.divisaoPorZero = false;
+
+     if (this.operadorSelecionado === '/' && this.numero2 === 0) {
+        this.resultado = null; // Evita mostrar número incorreto
+        this.divisaoPorZero = true;
+        return;
+    }
+
+    switch (this.operadorSelecionado){
+      case '+':
+        this.resultado = this.numero1 + this.numero2;
+        break;
+      case '-':
+        this.resultado = this.numero1 - this.numero2;
+        break;
+      case '*':
+        this.resultado = this.numero1 * this.numero2;
+        break;
+      case '/':
+        this.resultado = this.numero1 / this.numero2;
+        break;
+
+    }
   }
+
+
 }
