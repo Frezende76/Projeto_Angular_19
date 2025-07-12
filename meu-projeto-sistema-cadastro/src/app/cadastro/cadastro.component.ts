81c77948d36a164888fa0bec49563bbc7ca5f44f
Fabricio Rezende <fabriciorez@gmail.com>
2025-07-12 16:31:19 -0300

    Update: Criado meu arquivo classe, criado um services e feito o processo de salvar os dados via localStorage

--- meu-projeto-sistema-cadastro/src/app/cadastro/cadastro.component.ts
+++ meu-projeto-sistema-cadastro/src/app/cadastro/cadastro.component.ts
@@ -5,7 +5,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input'
 import { FormsModule } from '@angular/forms'
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule }  from '@angular/material/button'
+import { MatButtonModule }  from '@angular/material/button';
+import { Cliente } from './cliente';
+import { ClienteService } from '../cliente.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -23,4 +25,13 @@ import { MatButtonModule }  from '@angular/material/button'
 })
 export class CadastroComponent {
 
+    cliente: Cliente = Cliente.newCliente();
+
+    constructor(private service: ClienteService){
+
+    }
+
+    salvar(){
+      this.service.salvar(this.cliente);
+    }
 }
